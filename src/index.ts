e6d856a24501be57172d09bd48441c46587f4d69
Edgardo Martinez <edkiz93@gmail.com>
2022-02-27 12:24:00 -0500

    Actualizacion del index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import app from "./app";
 
 const port = app.get('port')
 
+process.on('uncaughtException', function (err) {
+    console.log('Error Capturado: ', err.stack);
+});
+
 app.listen(port)
 console.log('Server listening on port',port)
