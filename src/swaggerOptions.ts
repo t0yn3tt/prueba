7728322a846b1aa4b1c087b04a077381a64363c8
Edgardo Martinez <edkiz93@gmail.com>
2022-02-27 12:12:02 -0500

    Actualizacion del swaggerOptions

--- src/swaggerOptions.ts
+++ src/swaggerOptions.ts
@@ -8,7 +8,7 @@ export const options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000'
+                url: 'https://prueba-personas.herokuapp.com/'
             }
         ]
     },
